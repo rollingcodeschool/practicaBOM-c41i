55ca3cfd88acf2d76d65bfafa519c53245d82039
Emilse Arias <emilsearias@gmail.com>
2023-04-12 16:53:28 -0300

    agregamos funcionalidad del reloj

--- js/reloj.js
+++ js/reloj.js
@@ -1,14 +1,61 @@
-let fechaActual = new Date();
-console.log(fechaActual);
-console.log(fechaActual.getDate()); // 1-31
-console.log(fechaActual.getDay()); // 0-6
-console.log(fechaActual.getMonth()); // 0-11
-console.log(fechaActual.getSeconds()); // 0-11
+function obtenerFechaHora(){
+    let fechaActual = new Date();
+    // console.log(fechaActual);
+    // console.log(fechaActual.getDate()); // 1-31
+    // console.log(fechaActual.getDay()); // 0-6
+    // console.log(fechaActual.getMonth()); // 0-11
+    // console.log(fechaActual.getSeconds()); // 0-11
+    
+    let diaSemanas = [
+      "Domingo",
+      "Lunes",
+      "Martes",
+      "Miercoles",
+      "Jueves",
+      "Viernes",
+      "Sabado",
+    ];
+    let mesesAnio = [
+      "Enero",
+      "Febrero",
+      "Marzo",
+      "Abril",
+      "Mayo",
+      "Junio",
+      "Julio",
+      "Agosto",
+      "Septiembre",
+      "Octubre",
+      "Noviembre",
+      "Diciembre",
+    ];
+    
+    console.log(diaSemanas[fechaActual.getDay()]);
+    // Lunes 10 de Abril del 2023
+    let parrafoFecha = document.querySelector("#fecha");
+    let parrafoHora = document.querySelector("#hora");
+    parrafoFecha.innerHTML = `${
+      diaSemanas[fechaActual.getDay()]
+    } ${fechaActual.getDate()} de ${
+      mesesAnio[fechaActual.getMonth()]
+    } del ${fechaActual.getFullYear()}`;
+    
+    let hora = fechaActual.getHours(),
+      minutos = fechaActual.getMinutes(),
+      segundos =fechaActual.getSeconds();
+    
+      if(segundos < 10){
+        segundos = '0' + segundos
+      }
+      if(minutos < 10){
+        minutos = '0' + minutos
+      }
+      if(hora < 10){
+        hora = '0' + hora
+      }
+    
+    parrafoHora.innerHTML = `${hora}:${minutos}:${segundos}`;
+    
+}
 
-let diaSemanas = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
-let mesesAnio = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
-
-console.log(diaSemanas[fechaActual.getDay()]);
-// Lunes 10 de Abril del 2023
-let parrafoFecha = document.querySelector('#fecha');
-parrafoFecha.innerHTML = `${diaSemanas[fechaActual.getDay()]} ${fechaActual.getDate()} de ${mesesAnio[fechaActual.getMonth()]} del ${fechaActual.getFullYear()}`
+setInterval(obtenerFechaHora,1000);
